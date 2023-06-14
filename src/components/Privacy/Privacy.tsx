e090b5a40df49380cdf9d8e865c0bf4b54fbeafa
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-06-14 16:29:23 +0300

    style изменил privacy * Изменил вёрстку tsx * Поправил стили у privacy чтобы они корректно работали

--- src/components/Privacy/Privacy.tsx
+++ src/components/Privacy/Privacy.tsx
@@ -3,14 +3,14 @@ import { PrivacyProps } from './Privacy.types';
 
 export function Privacy({ }: PrivacyProps) {
     return (
-        <div className={s.privacy}>
-            <input
-                className={s.privacy__checkbox}
-                type="checkbox"
-                id="check"
-            />
-            <label htmlFor="check" className={s.privacy__agree}></label>
-            <label htmlFor="check" className={s.privacy__label}>Я принимаю политику конфиденциальности и пользовательское соглашение</label>
-        </div>
+        <label className={s.privacy}>
+                <input
+                    className={s.privacy__checkbox}
+                    type="checkbox"
+                    id="check"
+                />
+                <span className={s.privacy__agree}></span>
+                <span className={s.privacy__label}>Я принимаю политику конфиденциальности и пользовательское соглашение</span>
+        </label>
     );
 };
