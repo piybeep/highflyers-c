6839f93d4f6a4436f33ff1a0f378ce0fabfdf7be
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-06-19 23:54:33 +0300

    *fix: nextAuth * Попытались пофиксить, не пофиксили

--- src/components/AuthGoogle/AuthGoogle.tsx
+++ src/components/AuthGoogle/AuthGoogle.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
 
 import Image from 'next/image';
@@ -9,10 +10,12 @@ import { AuthGoogleProps } from './AuthGoogle.types';
 
 import icon from '../../../public/img/GoogleLogo.png'
 
-export function AuthGoogle({...props}:AuthGoogleProps) {
+export function AuthGoogle({ ...props }: AuthGoogleProps) {
+    const searchParams = useSearchParams()
+    const callbackUrl = searchParams.get('callbackUrl') || '/'
     return (
-        <button onClick={() => signIn()} {...props} className={s.button}>
-            <Image width={36} height={36} className={s.button__icon} src={icon.src} alt={'иконка'}/>
+        <button onClick={() => signIn('google', { callbackUrl })} {...props} className={s.button}>
+            <Image width={36} height={36} className={s.button__icon} src={icon.src} alt={'иконка'} />
             <h3 className={s.button__text}>Авторизоваться с Google</h3>
         </button>
     );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,69 @@
-import type { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import axios from "axios";
 
 export const authOptions: NextAuthOptions = {
+    pages: {
+        signIn: '/authorization',
+    },
     session: {
         strategy: "jwt",
     },
     providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
         CredentialsProvider({
+            id: 'credentials',
+            type: 'credentials',
             name: "Sign in",
             credentials: {
                 email: {
-                    label: "Email",
-                    type: "email",
-                    placeholder: "example@example.com",
+                    label: 'Почта',
+                    type: 'email',
+                    placeholder: 'Введите адрес электронной почты',
                 },
-                password: { label: "Password", type: "password" },
+                password: {
+                    label: 'Пароль',
+                    type: 'password',
+                    placeholder: 'Введите пароль',
+                }
             },
             async authorize(credentials) {
-                const user = { id: "1", name: "Admin", email: "admin@admin.com" };
-                return user;
+                if (credentials?.email && credentials.password) {
+                    const response = await axios.put(`${process.env.NEXT_PUBLIC_HOST}/auth`, {
+                        email: credentials.email,
+                        password: credentials.password
+                    });
+                    return response.data
+                }
+                return null
             },
         }),
     ],
+    callbacks: {
+        session: ({ session, token }) => {
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.id ?? token.sub
+                }
+            }
+        },
+        jwt: ({ token, user }) => {
+            if (user) {
+                const data = user as unknown as any
+                return {
+                    ...token,
+                    email: data.user.email,
+                    name: `${data.user.first_name ?? ""} ${data.user.second_name ?? ""}`.trim(),
+                    id: data.user.id,
+                }
+            }
+            return token
+        }
+    }
 };
