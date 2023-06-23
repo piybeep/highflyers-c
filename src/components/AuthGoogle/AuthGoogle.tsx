2aebc2fa481338317e3aa4e8adcf14d8409c7fdd
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-06-23 17:55:41 +0300

    fix: Переделали авторизацию на <googleLogin/>

--- src/components/AuthGoogle/AuthGoogle.tsx
+++ src/components/AuthGoogle/AuthGoogle.tsx
@@ -1,32 +1,36 @@
 'use client'
 
-import { useGoogleLogin } from '@react-oauth/google';
+import { toast } from 'react-hot-toast';
+
+import { useRouter } from 'next/navigation';
+
+import { GoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 
-import Image from 'next/image';
-
-import s from './AuthGoogle.module.scss'
 import { AuthGoogleProps } from './AuthGoogle.types';
 
-import icon from '../../../public/img/GoogleLogo.png'
-
-export function AuthGoogle({ ...props }: AuthGoogleProps) {
-
-    const googleLogin = useGoogleLogin({
-        onSuccess: async ({ code }) => {
-            const tokens = await axios.post(`${process.env.NEXT_PUBLIC_HOST}/auth`, {
-                code,
-            });
-
-            
-        },
-        onError: errorResponse => console.log(errorResponse),
-        flow: 'auth-code',
-    });
+export function AuthGoogle({ }: AuthGoogleProps) {
+    const route = useRouter()
     return (
-        <button onClick={() => googleLogin()} {...props} className={s.button}>
-            <Image width={36} height={36} className={s.button__icon} src={icon.src} alt={'иконка'} />
-            <h3 className={s.button__text}>Авторизоваться с Google</h3>
-        </button>
+        <GoogleLogin
+            onSuccess={async (credentialResponse) => {
+                axios.post(
+                    `${process.env.NEXT_PUBLIC_HOST}/auth/google`,
+                    {
+                        token: credentialResponse.credential,
+                    }
+                ).then((data) => {
+                    console.log(data.data)
+                    route.push('/')
+                })
+                    .catch((error) => {
+                        toast.error('Что-то пошло не так')
+                        console.error(error)
+                    })
+            }}
+            onError={() => {
+                console.log("Login Failed");
+            }}
+        />
     );
 };
