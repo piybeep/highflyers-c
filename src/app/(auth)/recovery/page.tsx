a65e3b5df25002bc6050b456f0d6ed12b114392d
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-06-27 06:22:31 +0300

    fix: Исправил recovery page:Page

--- src/app/(auth)/recovery/page.tsx
+++ src/app/(auth)/recovery/page.tsx
@@ -3,7 +3,7 @@
 import { Recovery, Submit } from "@/modules";
 import { useState } from "react";
 
-export default function page() {
+export default function Page() {
     const [isSubmited, setIsSubmited] = useState(false)
     return (
         <>
