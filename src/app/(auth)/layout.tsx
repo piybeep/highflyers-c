ea02a96001c72d6f0b0c8295b5f2cf5650edee72
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-06-27 06:20:01 +0300

    fix: Переиминовал segment:useSegment * Переименовал layout на Layout * Переименовал segment на useSegment

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,11 +4,11 @@ import { useSelectedLayoutSegment } from 'next/navigation'
 import { PropsWithChildren } from 'react';
 import s from './layout.module.scss'
 
-export default function layout({ children }: PropsWithChildren) {
-    const segment = useSelectedLayoutSegment()
+export default function Layout({ children }: PropsWithChildren) {
+    const useSegment = useSelectedLayoutSegment()
     return (
         <div className={s.wrapper} style={{
-            backgroundImage: `${segment === 'authorization' ? 'url(/img/authorizationBg.png)' : 'url(/img/registrationBg.png)'}`
+            backgroundImage: `${useSegment === 'authorization' ? 'url(/img/authorizationBg.png)' : 'url(/img/registrationBg.png)'}`
         }} >
             <div className={s.container}>
                 {children}
