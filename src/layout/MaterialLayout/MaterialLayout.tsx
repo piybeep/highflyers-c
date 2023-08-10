7862731793a6a168aa1ccb0fee09f5a580b2d444
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-08-10 20:34:41 +0300

    feat: *Создал функцию для рандомного цвета карточки в двух вариантах:    * Одна выбор из массива    * Вторая полностью случайный цвет

--- src/layout/MaterialLayout/MaterialLayout.tsx
+++ src/layout/MaterialLayout/MaterialLayout.tsx
@@ -7,9 +7,29 @@ interface MaterialLayoutProps {
     materialTypes: 'check' | 'plans' | 'article'
     children?: JSX.Element | JSX.Element[]
 }
+
 export function MaterialLayout({ children, materialTypes }: MaterialLayoutProps) {
+    // const colors = ["green", "purple", "teal", "violet", "pink"]
+
+    const handleMouseEnter = (e: any) => {
+        // let randomNumber = Math.floor(Math.random() * colors.length)
+        // e.target.style.backgroundColor = `${colors[randomNumber]}`
+
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        e.target.style.backgroundColor = `${color}`
+    }
+
+    const handleMouseLeave = (e: any) => {
+        e.target.style.backgroundColor = ''
+    }
+
     return (
-        <div className={classNames(s.wrapper, {
+        <div onMouseEnter={e => handleMouseEnter(e)} onMouseLeave={e => handleMouseLeave(e)} className={classNames(s.wrapper, {
             [s.wrapper__plans]: materialTypes === 'plans',
             [s.wrapper__check]: materialTypes === 'check',
             [s.wrapper__article]: materialTypes === 'article',
