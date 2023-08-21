7ee3489a6a2d267d18e1e1d62af3471416f5c670
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-08-22 02:51:12 +0300

    feat: *Добавлена авторизация через google *Исправлен header у авторизованного и не авторизованного пользователя, но ошибка с гидрадацией *fix * Исправлен store.user

--- src/store/user/user.ts
+++ src/store/user/user.ts
@@ -2,9 +2,11 @@ import { create } from "zustand";
 
 import { userProps } from "./user.types";
 
-export const useUser = create<userProps>((set) => ({
+import { persist } from 'zustand/middleware'
+
+export const useUser = create<userProps>()(persist((set) => ({
     user: null,
     isAuth: false,
     setIsAuth: (flag: boolean) => set({ isAuth: flag }),
     setUser: (user) => set({ user }, true),
-}))
+}), { name: 'user' }))
