028a962941578fb3308e2651f2d0bc5c64fb7ca2
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-09-04 22:12:34 +0300

    fix: исправлены margin у TedTalks feat: сортировка по темам у TedTalks

---
+++ src/constants/data.ts
@@ -0,0 +1,85 @@
+import { TedTalksProps } from '@/modules/TedTalksList/TedTalksList.types'
+import img from '../../public/img/cover.png'
+
+export const dataTedTalks: TedTalksProps[] = [
+    {
+        id: '1',
+        theme: 'Грамматика',
+        name: 'Правильные и неправильные глаголы',
+        read_time: '50 минут',
+        link: '#',
+        preview: img.src,
+        tags: [
+            {
+                id: '1',
+                name: '# about language',
+                value: '# about language',
+            },
+            {
+                id: '2',
+                name: 'about language',
+                value: 'about language',
+            }
+        ]
+    },
+    {
+        id: '2',
+        theme: 'Фонетика',
+        name: 'Правильные и неправильные глаголы',
+        read_time: '50 минут',
+        link: '#',
+        preview: img.src,
+        tags: [
+            {
+                id: '1',
+                name: '# about language',
+                value: '# about language',
+            },
+            {
+                id: '2',
+                name: 'about language',
+                value: 'about language',
+            }
+        ]
+    },
+    {
+        id: '3',
+        theme: 'Фонетика',
+        name: 'Правильные и неправильные глаголы',
+        read_time: '50 минут',
+        link: '#',
+        preview: img.src,
+        tags: [
+            {
+                id: '1',
+                name: '# about language',
+                value: '# about language',
+            },
+            {
+                id: '2',
+                name: 'about language',
+                value: 'about language',
+            }
+        ]
+    },
+    {
+        id: '4',
+        theme: 'Грамматика',
+        name: 'Правильные и неправильные глаголы',
+        read_time: '50 минут',
+        link: '#',
+        preview: img.src,
+        tags: [
+            {
+                id: '1',
+                name: '# about language',
+                value: '# about language',
+            },
+            {
+                id: '2',
+                name: 'about language',
+                value: 'about language',
+            }
+        ]
+    },
+]
---
+++ src/modules/TedTalksList/TedTalksList.types.ts
@@ -0,0 +1,15 @@
+export interface TedTalksProps {
+    id: string,
+    name: string,
+    theme: string,
+    read_time: string,
+    link: string,
+    preview: string,
+    tags: TedTalksTags[]
+}
+
+export interface TedTalksTags {
+    id: string,
+    name: string,
+    value: string,
+}
