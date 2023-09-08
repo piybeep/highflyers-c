8c8f1f31fd00277728e7c88b2a164ac2eb3b8d19
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-09-08 17:20:17 +0300

    fix: исправил paralax

--- src/components/Paralax/Paralax.tsx
+++ src/components/Paralax/Paralax.tsx
@@ -5,28 +5,29 @@ import Image from 'next/image';
 import s from './Paralax.module.scss'
 
 import site from '../../../public/img/paralax/paralaxSite.png'
+import ipad from '../../../public/svg/paralax/IpadBg.svg'
 import { useEffect, useRef } from 'react';
 
 export function Paralax() {
-
     const refWrapper = useRef<any>()
-    const refImg = useRef<any>()
+    const refIpad = useRef<any>()
+    const refSite = useRef<any>()
 
     useEffect(() => {
         if (window) {
-            window.addEventListener('scroll', e => {
-                const paralaxHeight = refWrapper.current.clientHeight
-                const paralaxScrollTop = refWrapper.current.offsetTop
-                const wrapperPadding = 28 * 2
+            window.addEventListener('scroll', () => {
+                const wrapperScrollTop = refWrapper.current.offsetTop
 
-                const imgHeight = refImg.current.clientHeight
+                const ipadHeight = refIpad.current.clientHeight
 
-                // Рабочий вариант
-                // const top = (paralaxScrollTop - paralaxHeight) - window.scrollY
-                // Тестовый вариант
-                const top = (paralaxScrollTop - paralaxHeight / 2) - window.scrollY - wrapperPadding
+                const siteHeight = refSite.current.clientHeight
+
+                const marginSIte = parseInt(window.getComputedStyle(refSite.current).margin) * 2
+
+                const top = (wrapperScrollTop - ipadHeight / 2) - window.scrollY - marginSIte
                 const topZero = top < 0
-                const topHeight = top > (paralaxHeight - imgHeight - wrapperPadding)
+                const topHeight = top > ipadHeight - siteHeight - marginSIte
+
                 if (topZero && topHeight) {
                     document.body.style.cssText = `--scrollTop: ${top}px`
                 }
@@ -35,9 +36,9 @@ export function Paralax() {
     }, [])
 
     return (
-        <div ref={refWrapper} className={s.wrapper}>
-            <div className={s.wrapper__ipad}></div>
-            <Image ref={refImg} className={s.wrapper__img} src={site.src} alt={'картинка'} width={770} height={1045} />
-        </div>
+        <div ref={refWrapper} className={s.wrapper} >
+            <Image ref={refIpad} className={s.wrapper__ipad} src={ipad.src} alt={'картинка'} width={621} height={815} />
+            <Image ref={refSite} src={site.src} alt={'картинка'} className={s.wrapper__site} width={770} height={1045} />
+        </div >
     );
 };
