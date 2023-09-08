02016dc0dee236242db2b24371a36243ae259927
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2023-09-08 15:45:51 +0300

    feat: Добавил react hook form для формы на главной style: Добавил hover для authButton

--- src/modules/Form/Form.tsx
+++ src/modules/Form/Form.tsx
@@ -1,7 +1,25 @@
+'use client'
+
 import { AuthButton, FormInput, Privacy } from '@/components';
 import s from './Form.module.scss';
+import { Controller, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 export function Form() {
+
+    const { handleSubmit, control, reset } = useForm()
+
+    const onSubmit = async (values: any) => {
+        if (values.privacy) {
+            console.log(values)
+            toast.success('Форма успешно отправлена')
+            reset()
+        } else {
+            toast.error('Примите политику конфиденциальности')
+            console.log(values)
+        }
+    }
+
     return (
         <div className={s.wrapper}>
             <div className={s.info}>
@@ -13,21 +31,51 @@ export function Form() {
                 </p>
             </div>
 
-            <form action='#' className={s.form}>
+            <form action='#' className={s.form} onSubmit={handleSubmit(onSubmit)}>
                 <div className={s.form__inputs}>
-                    <FormInput placeholder={'Имя'} icon={'name'} />
-                    <FormInput
-                        placeholder={'Электронная почта'}
-                        icon={'email'}
-                        type='email'
+                    <Controller
+                        control={control}
+                        name="name"
+                        render={({ field: { onChange, value } }) => (
+                            <FormInput placeholder={'Имя'} icon={'name'} value={value ?? ''} onChange={onChange} required />
+                        )}
                     />
-                    <FormInput
-                        placeholder={'Какой у вас вопрос?'}
-                        icon={'question'}
+                    <Controller
+                        control={control}
+                        name="email"
+                        render={({ field: { onChange, value } }) => (
+                            <FormInput
+                                value={value ?? ''}
+                                onChange={onChange}
+                                placeholder={'Электронная почта'}
+                                icon={'email'}
+                                required
+                                type='email'
+                            />
+                        )}
+                    />
+                    <Controller
+                        control={control}
+                        name="question"
+                        render={({ field: { onChange, value } }) => (
+                            <FormInput
+                                value={value ?? ''}
+                                onChange={onChange}
+                                placeholder={'Какой у вас вопрос?'}
+                                icon={'question'}
+                            />
+                        )}
                     />
                 </div>
                 <div className={s.form__info}>
-                    <Privacy />
+                    <Controller
+                        control={control}
+                        name="privacy"
+                        defaultValue={false}
+                        render={({ field: { onChange, value } }) => (
+                            <Privacy onChange={onChange} checked={value} />
+                        )}
+                    />
                     <AuthButton
                         type='submit'
                         isOutline={true}
@@ -36,6 +84,6 @@ export function Form() {
                     />
                 </div>
             </form>
-        </div>
+        </div >
     );
 }
