53bc63d071a965bcee3f21c0469e2ad43d29dd58
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-01-26 06:22:42 +0300

    fix: исправил кэш, просто убрал кэширование, вроде как оно заработало

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 export default axios.create({
     baseURL: process.env.NEXT_PUBLIC_HOST,
     headers: {
-        Authorization: `Bearer ${process.env.API_TOKEN}`
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Expires': '0',
     }
 });
