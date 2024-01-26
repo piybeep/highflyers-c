2e9526ccaf2e5afbdc5b0ccff79b9f95add16264
Wycctrujjla109121 <56939296+Wycctrujjla109121@users.noreply.github.com>
2024-01-26 06:12:32 +0300

    feat: вывод в модуле preview только те страницы, которые есть в базе

--- src/app/(highflyers)/page.tsx
+++ src/app/(highflyers)/page.tsx
@@ -14,12 +14,40 @@ import {
 } from '@/modules';
 
 import s from './page.module.scss';
+import api from '@/utils/api';
+import { infoViewKeys } from '@/types';
 
 export default async function Home() {
     const res = MATERIALS_LIST;
+
+    const [resLearning, resArticles, resLessonPlans, resCheckLists] = await Promise.all([
+        api.get(`cards`)
+            .then(res => res.data.data)
+            .catch(error => console.error(error)),
+
+        api.get(`articles`)
+            .then(res => res.data.data)
+            .catch(error => console.error(error)),
+
+        api.get(`lesson-plans`)
+            .then(res => res.data.data)
+            .catch(error => console.error(error)),
+
+        api.get(`check-lists`)
+            .then(res => res.data.data)
+            .catch(error => console.error(error)),
+    ])
+
+    const infoView: Record<infoViewKeys, boolean> = {
+        'Обучение по карточкам': !!resLearning,
+        'Полезные статьи': !!resArticles,
+        'Планы уроков': !!resLessonPlans,
+        'Чек-листы': !!resCheckLists,
+    }
+
     return (
         <main className={s.wrapper}>
-            <Preview />
+            <Preview previewInfoView={infoView} />
             <Started />
             <Lessons />
             <Course />
